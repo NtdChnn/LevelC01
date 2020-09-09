31ba6c944aefa23f39b3fa6ffb02237eaf9059c4
NtdChnn <69079640+NtdChnn@users.noreply.github.com>
2020-09-09 14:30:21 +0700

    Add project files.

---
+++ LevelC01/Source.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int x;
+	scanf_s("%d", &x);
+	for (int i = 1; i <= x; i++)
+	{
+		for (int j = 1; j <= x; j++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
